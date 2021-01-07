29a57e5f7ccb15c7c95f4f470d120a539b6b25c4
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-01-07 11:01:02 +0000

    intial upload with basic setup

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Main.css";
+
+export default function Main() {
+  return <main className="Main">Main Stuff</main>;
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return <aside className="Sidebar">Sidebar</aside>;
+}
