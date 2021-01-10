82015aa51c139de6a3d2695b227fbc0a76118e5c
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-01-10 14:53:32 +0000

    remove comments

--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -19,11 +19,6 @@ export default function MessageInput({ value }) {
     const { typing, activeUserId, activeEditingMessage } = state;
     const { messageId, userId } = activeEditingMessage;
 
-    //we want
-    // [x] if value is empty, do nothing
-    // if value is the same, don't add "edited"
-    // if value is not the same, add "edited"
-
     if (value) {
       if (activeEditingMessage.messageId) {
         store.dispatch(updateMessage(typing, messageId, userId));
